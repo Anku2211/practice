22321f39a26237496d7b06c0c049c467d6ef0a3b
Chetan Sachdev <mail@chetansachdev.com>
2020-11-23 18:28:12 +0530

    Included functionality to convert base64 string to image

--- mammoth/index.js
+++ mammoth/index.js
@@ -1,13 +1,19 @@
 var fs = require('fs-extra');
-const image = require('./wordhtml.txt');
+var base64Img = require('base64-img');
+// fs.readFile('./wordhtml.txt', function (err, data) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   data = data.toString();
+//   data = data.replace(/[^:/]\w+(?=;|,)/ [0], '');
+//   fs.writeFile('output/output.txt', data, function () {
+//     console.log(data);
+//   });
+// });
 
-fs.readFile('./wordhtml.txt', function (err, data) {
-  if (err) {
-    console.log(err);
-  }
-  data = data.toString();
-  data = data.replace(/[^:/]\w+(?=;|,)/[0], '');
-  fs.writeFile('output/output.txt', data, function () {
-    console.log(data);
-  });
+const data = fs.readFileSync('./wordhtml.txt', {
+  encoding: 'utf-8'
 });
+var filepath = base64Img.imgSync(data, './output/', 'test');
+
+console.log(filepath);
